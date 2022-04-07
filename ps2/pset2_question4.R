d2bf86e61d233ae9b0deb33e99a392e745b9ab2d
Alexander Weinberg <weinberga421@gmail.com>
2022-04-07 10:19:27 -0500

    q4-part b

--- ps2/pset2_question4.R
+++ ps2/pset2_question4.R
@@ -14,89 +14,37 @@ summary(dt$Y) # median/mean income = 39k and 41k
 # max income 142k, min = 2
 
 
+## PARAMETERS ---------------
+
+delta   <- 2000  # 1/2 size of excluded range
+K       <- 20000 # kink location
+
+
 ## PART A: PLOT HISTOGRAM ---------------
 ggplot() +
-  geom_histogram(data=dt, aes(x=Y)) +
-  scale_y_continuous(limits=c(0,150000), 
+  geom_histogram(data=dt, aes(x=Y), binwidth = delta) +
+  scale_y_continuous(limits=c(0,100000), 
                      breaks = scales::breaks_width(50000), 
                      labels = scales::comma) +
   scale_x_continuous(labels=scales::dollar) +
   theme_minimal() + 
   labs(x="Income", y="Count", title="Distribution of Income") 
 
+## PART B: Bunching Probability ---------------
+y1 <- K - delta # low bound of excluded range
+y2 <- K + delta # upper bound of excluded range
 
+# Non-parametric estimator of f_(y1), f+(y2)
+N_f1  <- dt[between(Y,y1 - delta, y1),.N]
+N_f2  <- dt[between(Y,y2, y2 + delta),.N]
+N_excl <- dt[between(Y,y1,y2),.N]
+N_denom <- dt[between(Y,y1 - delta, y2 + delta),.N]
 
-ggplot(data=CT19, aes(age, fill=race)) + 
-  geom_histogram(breaks=seq(15, 80, by = 1)) +
-  theme_minimal()+ theme(text=element_text(family="Palatino"))+
-  scale_fill_manual(name="Race", values = cbPalette) +  
-  theme(plot.title = element_text(hjust = 0))+
-  labs(x="Age", y="Number of Pretrial Inmates", caption="This graphs covers only the 3,230 inmates present in DOC facilities on 7/28/19.")+
-  scale_y_continuous(limits=c(0,126), breaks=seq(0,126,25))+
-  scale_x_continuous(limits=c(15,81), breaks=seq(15,80,5))+ 
-  ggtitle("Age and Race of Connecticut Pretrial Inmates (7/28/2019)", subtitle = "Data Available via Connecticut Open Data")
+fminus <- N_f1 / (delta * N_denom)
+fplus  <- N_f2 / (delta * N_denom)
 
+# Implied P_K
+Pk <- (N_excl / N_denom) - delta * (fminus + fplus)
 
-## Merge Scorecard and Test Optional DATA -----------------------
-dt <- merge(dt,TOdata, by='UNITID', all.x=TRUE)
-  
+## PART C: Taxable Elasticity ---------------
 
-## CLEAN AND PREP -----------------------
-
-# Define treatment vars
-dt[, year_since_treatment := year - year_switch] # relative time
-
-# Categorize
-dt[, treated := if_else(year_switch < 2020, 'treated pre covid','treated 2020')]
-dt[is.na(year_switch), 'treated'] = 'never treated'
-
-## DESCRIPTIVE STATS -----------------------
-aux_mean <- function(x) mean(x, na.rm=TRUE) # drop na mean function
-
-# COMPARE TREATED VS. NEVER TREATED VS. TREATED 2020
-dt_summarystats <- dt %>%
-  filter(year==2011) %>%
-  group_by(treated) %>%
-  mutate(N=n()) %>% 
-  summarize_all(aux_mean)
-
-dt_summarystats <- data.frame(t(dt_summarystats))
-dt_summarystats <- drop_na(dt_summarystats)
-
-dt_summarystats <- dt_summarystats %>%
-  rename(`Test Required` = X1, `Test optional during 2020` = X2, `Test Optional prior to 2020` = X3) 
-
-stargazer(dt_summarystats, type='latex', digits = 2,
-          title='Summary statistics for 2011',
-          out='../output/tabs/descriptive.tex', 
-          summary=FALSE)
-
-
-# COMPARE COHORTS WITHIN TREATED < 2020
-dt_summarystats <- dt[treated=='treated pre covid'] # only schools TO pre covid
-
-dt_summarystats <- dt_summarystats %>%
-  filter(year==2011) %>%
-  filter(between(year_switch, 2011, 2019)) %>%
-  group_by(year_switch) %>%
-  mutate(N=n()) %>% 
-  summarize_all(aux_mean) %>%
-  select_if(~ !any(is.na(.)))
-
-dt_summarystats <- data.frame(t(dt_summarystats)) # transpose
-dt_summarystats <- drop_na(dt_summarystats)
-
-stargazer(dt_summarystats, type='latex', digits = 2,
-          title='Summary statistics for 2011 by Cohort',
-          out='../output/tabs/descriptive_cohort.tex', summary=FALSE)
-
-
-## SAMPLE SELECTION -----------------------
-save(dt, file='../output/full.Rdata')
-dt <- dt[treated=='treated pre covid'] # only schools TO pre covid
-dt <- dt[between(year,2011,2019)] # restrict 2011-2019
-dt <- dt[!is.na(adm_rate)]
-dt <- dt[!is.na(avg_cost)]
-
-## Save out cleaned data -----------------------
-save(dt, file='../output/main.Rdata')
